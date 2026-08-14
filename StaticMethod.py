9bef1444d5eabc11cbbeda26641bdd347259e725
RavindraPatole <ravindra51patole@gmail.com>
2026-08-14 12:50:46 +0530

    Update StaticMethod.py
    Car type added

--- StaticMethod.py
+++ StaticMethod.py
@@ -1,7 +1,7 @@
 class car:
     @staticmethod
     def Ctype(type_, seats, size):
-        print(f" car is having type{type_} and seats {seats} capacity and size {size} of car")
+        print(f" this car is having type{type_} and seats {seats} capacity and size {size} of car")
 
 
 car.Ctype("honda", "7", "five")
